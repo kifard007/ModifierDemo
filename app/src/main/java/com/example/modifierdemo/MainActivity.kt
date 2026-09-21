dfb72e3af6c17a33b964df130b8101efb11675a7
kifard007 <161320917+kifard007@users.noreply.github.com>
2026-09-21 14:03:53 +0700

    DemoScreen

--- app/src/main/java/com/example/modifierdemo/MainActivity.kt
+++ app/src/main/java/com/example/modifierdemo/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -12,6 +13,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.modifierdemo.ui.theme.ModifierDemoTheme
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,11 +24,22 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ModifierDemoTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) {
-
-                }
+                DemoScreen();
             }
         }
+    }
+    @Composable
+    fun DemoScreen() {
+        val mymodifier = Modifier
+            .border(width = 2.dp, color = Color.Black)
+            .padding(all = 10.dp)
+
+        Text(
+            "Hello Compose",
+            modifier = mymodifier,
+            fontSize = 40.sp,
+            fontWeight = FontWeight.Bold
+        )
     }
 }
 
